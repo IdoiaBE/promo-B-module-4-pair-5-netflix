c92e83fa21c02a11af935a5a8542890ecfe55c64
IdoiaBE <1992ibe@gmail.com>
2025-02-13 11:29:59 +0100

    new queries

--- bd/NetflixQueries.sql
+++ bd/NetflixQueries.sql
@@ -17,3 +17,17 @@ SELECT name, lastname FROM Actors WHERE country = 'Estados Unidos';
 SELECT * FROM Users WHERE plan_details = 'Standard';
 
 DELETE FROM Users WHERE name like 'M%';
+
+-- Cuantas pelis favoritas por usuario
+SELECT users.name, count(FK_Movies) as numero_favoritas
+FROM users inner join users_movies on users.idUser = users_movies.FK_Users
+GROUP BY FK_Users;
+
+-- Mostrar user con mayor cantidad de pelis favoritas
+SELECT users.name, count(FK_Movies) as numero_favoritas
+FROM users inner join users_movies on users.idUser = users_movies.FK_Users
+GROUP BY FK_Users
+HAVING numero_favoritas >1;
+
+select * from users_movies;
+
