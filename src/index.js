d50eacd22de68e121c50090b70b6a1a55c11a107
IdoiaBE <1992ibe@gmail.com>
2025-02-19 10:58:37 +0100

    mysql connection

--- src/index.js
+++ src/index.js
@@ -1,45 +1,87 @@
 const express = require('express');
 const cors = require('cors');
+const mysql = require("mysql2/promise");
 
 // create and config server
 const server = express();
 server.use(cors());
-server.use(express.json());
+server.use(express.json({limit: "50mb"}));
 
-const fakeMovies = [
-  {
-    id: 1,
-    title: "Wonder Woman",
-    genre: "Action",
-    image:
-      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
-    category: "Superhero",
-    year: 2017,
-    director: "Patty Jenkins",
-  },
-  {
-    id: 2,
-    title: "Inception",
-    genre: "Science Fiction",
-    image:
-      "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
-    category: "Thriller",
-    year: 2010,
-    director: "Christopher Nolan",
-  },
-];
+async function connectBD() {
+  const conex = await mysql.createConnection({
+      host: "localhost",
+      user: "root", 
+      password: "mysql",
+      database: "netflix",
+  });
+  conex.connect();
+  return conex;   
+}
 
-//enpoints
+
+// const fakeMovies = [
+//   {
+//     id: 1,
+//     title: "Wonder Woman",
+//     genre: "Action",
+//     image:
+//       "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2022/12/gal-gadot-como-wonder-woman-universo-extendido-dc-2895594.jpg?tf=3840x",
+//     category: "Superhero",
+//     year: 2017,
+//     director: "Patty Jenkins",
+//   },
+//   {
+//     id: 2,
+//     title: "Inception",
+//     genre: "Science Fiction",
+//     image:
+//       "https://m.media-amazon.com/images/S/pv-target-images/e826ebbcc692b4d19059d24125cf23699067ab621c979612fd0ca11ab42a65cb._SX1080_FMjpg_.jpg",
+//     category: "Thriller",
+//     year: 2010,
+//     director: "Christopher Nolan",
+//   },
+// ];
+
+//ENDPOINTS
 
 //escuchar las peticiones para pedir peliculas
 
-server.get("/movies", (req, res) => {
-  if(fakeMovies.length === 0){
-    res.status(404).json({success:false, error:"No se ha encontrado el recurso"});
-  } else {
-    res.status(200).json({success:true, movies: fakeMovies});
+// server.get("/movies", (req, res) => {
+//   if(fakeMovies.length === 0){
+//     res.status(404).json({success:false, error:"No se ha encontrado el recurso"});
+//   } else {
+//     res.status(200).json({success:true, movies: fakeMovies});
+//   }
+// })
+
+server.get("/movies", async (req, res)=>{  
+  try {
+      const connection = await connectBD();
+      const sqlSelect = "SELECT * FROM Movies";
+      const [result] = await connection.query(sqlSelect);
+      connection.end();
+
+      if(result.length === 0){
+          res.status (404).json({
+            success: false,
+            message: "No se encontraron películas"
+          });
+      }else{
+          res.status (200).json({
+            success: true,
+            movies:  result,
+          });
+      }
+
+  } catch (error) {
+      console.log(error);
+      res.status(500).json({    //500 = error en el servidor
+        success: false,
+        message: error,
+      });     
   }
-})
+});
+
 
 
 
